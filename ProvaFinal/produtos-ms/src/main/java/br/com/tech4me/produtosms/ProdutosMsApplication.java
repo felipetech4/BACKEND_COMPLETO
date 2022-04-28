32ea2c14e138d2d7f6ca10a6d1625faffd66be75
felipetech4me <97922422+felipetech4me@users.noreply.github.com>
2022-04-28 12:36:00 -0300

    felipeegabriel

---
+++ ProvaFinal/produtos-ms/src/main/java/br/com/tech4me/produtosms/ProdutosMsApplication.java
@@ -0,0 +1,13 @@
+package br.com.tech4me.produtosms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutosMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutosMsApplication.class, args);
+	}
+
+}
